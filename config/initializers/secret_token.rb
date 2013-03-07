de8d6fad5a1047d539a8d8a8c3fd9071d9152d89
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2013-03-07 22:04:29 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BadgershipService::Application.config.secret_token = 'b42074b8a5e3ee79b569a0b9f01576446b6457c1707e27490c416d7ff646d83721111c408660b59098f2de45b7b183f4be38815638d335abc46d09ca2f71e36d'
